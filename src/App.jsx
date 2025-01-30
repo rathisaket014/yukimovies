e178fed4823d39a0795bfbf88400f91ce93eb060
rathisaket014 <ice.westwind@gmail.com>
2025-01-30 20:59:00 +0530

    cloned into insiders

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import Card from './components/Card'
 
 const App = () => {
   return (
-    <div className='text-4xl font-semibold text-center p-5'>
-      <Card title="Star Wars" />
-      <Card title="John Wick" />
-      <Card title="Harry Potter" />
-    </div>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>Find <span>movies</span> without any hassle</h1>
+          </header>
+        </div>
+      </div>
+    </main>
   )
 }
 
